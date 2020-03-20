148d450f172c282243c4fa09d11195806358e1cc
bushbushsaur <56460158+bushbushsaur@users.noreply.github.com>
2020-03-20 15:24:45 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-var pages = [["Home","https://bushbushsaur.github.io/"],["About Me","aboutme.html"]];
-var nav = $("<nav>");
-for (var i = 0;i < pages.length;i++) {
-  var a = $("<a>");
-  a.text(pages[i][0]);
-  a.attr("href",pages[i][1]);
-  a.appendTo(nav);
-}
-nav.prependTo("body");
+
