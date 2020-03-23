d313423a5e35a71a7b7c2ee6d47a802d4aaae7c6
bushbushsaur <56460158+bushbushsaur@users.noreply.github.com>
2020-03-23 07:02:13 -0700

    Update main.js
    Updated navigation bar system to support submenus with this syntax:
    <span name="(name)">...</span>
    <div for="(name)"><a ... /a></div>

--- files/main.js
+++ files/main.js
@@ -1,10 +1,29 @@
 $(document).ready(function() {
-  window.setInterval(function() {
-    $("pre[syntax]").blur(function() {
-      $(this).text($(this).text());
-      $(this).syntax($(this).attr("syntax"));
+  $("nav span").click(function() {
+    var $span = $(this);
+    var $div = $("nav div[for=" + $span.attr("name") + "]");
+    var $parentSpans = $("");
+    var $parentDivs = $("");
+    $div.parents("div").each(function() {
+      $parentDivs = $parentDivs.add($(this));
+      $parentSpans = $parentSpans.add($("nav span[name=" + $(this).attr("for") + "]"));
     });
-  },2500);
+    $div.css("top",$span.offset().top + $span.height() + 21);
+    if ($div.is(":hidden")) {
+      $div.show();
+      $("nav span").removeClass("active");
+      $span.addClass("active");
+      $parentSpans.addClass("active");
+      $span.siblings("span").each(function() {
+        $("nav div[for=" + $(this).attr("name") + "]").hide();
+      });
+    } else {
+      $div.hide();
+      $div.find("div").hide();
+      $("nav span").removeClass("active");
+      $parentSpans.addClass("active");
+    }
+  });
   
   $("pre[syntax]").each(function() {
     $(this).syntax($(this).attr("syntax"));
